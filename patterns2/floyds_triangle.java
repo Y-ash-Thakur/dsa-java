78c85097224a8be05b02c9558b7a9da8ea8fa918
Y-ash-Thakur <yasht0509@gmail.com>
2026-06-05 13:36:50 +0530

    advance patterns, butterfly, floyds_triangle, zero one triangle.

---
+++ patterns2/floyds_triangle.java
@@ -0,0 +1,19 @@
+package patterns2;
+
+public class floyds_triangle {
+    public static void floyds_triangle(int n){
+        int number = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+            floyds_triangle(5);
+        }
+    
+}

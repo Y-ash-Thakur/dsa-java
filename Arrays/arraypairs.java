28ec649e244639e6a4c1ea3dc1e26764200d4324
Y-ash-Thakur <yasht0509@gmail.com>
2026-06-08 12:48:39 +0530

    printing sub arrays of the array

--- Arrays/arraypairs.java
+++ Arrays/arraypairs.java
@@ -4,9 +4,9 @@ public class arraypairs {
 
     public static void print_array_pairs(int[] numbers){
         for(int i = 0; i < numbers.length; i++){
-            int current_number = numbers[i];
+            // int current_number = numbers[i];
             for(int j = i + 1; j < numbers.length; j++){
-                System.out.println("( " + current_number + "," + numbers[j] + ")");
+                System.out.println("( " + numbers[i] + "," + numbers[j] + ")");
             }
             System.out.println();
         }
---
+++ Arrays/subarray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class subarray {
+    public static void print_subarray(int[] numbers){
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i; j < numbers.length; j++){
+                for(int k = i; k <= j; k++){
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int[] numbers = {2,4,6,8,10};
+        print_subarray(numbers);
+    }
+    
+}

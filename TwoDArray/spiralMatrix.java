4905680a1c4c7b85a8060bfda4b0a1bdc38484bc
Y-ash-Thakur <yasht0509@gmail.com>
2026-06-18 10:09:55 +0530

    2d array basic and spiral matrix question.

---
+++ TwoDArray/spiralMatrix.java
@@ -0,0 +1,50 @@
+package TwoDArray;
+
+public class spiralMatrix {
+    public static void spiral_matrix(int[][] matrix){
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while(startRow <= endRow && startCol <= endCol){
+
+            for(int j = startCol; j <= endCol; j++){
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            for(int i = startRow + 1; i <= endRow; i++){
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            for(int j = endCol - 1; j >= startCol; j--) {
+                if(startRow == endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            for(int i = endRow-1; i >= startRow+1; i--){
+                if(startCol == endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+        System.out.println();
+    }
+
+    static void main() {
+        int matrix[][] = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+        spiral_matrix(matrix);
+    }
+}

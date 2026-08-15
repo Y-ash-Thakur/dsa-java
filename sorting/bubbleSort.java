764e72174462a3b9285d739d9dc99a87015aae20
Y-ash-Thakur <yasht0509@gmail.com>
2026-08-16 00:01:12 +0530

    selection and bubble sort practice

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -1,12 +1,12 @@
 package sorting;
 
-import org.w3c.dom.ls.LSOutput;
+import java.util.*;
 
 public class bubbleSort {
-    public static void bubble_sort(int arr[]){
+    public static void bubble_sort(int [] arr){
         int n = arr.length;
-        for(int i = n-1; i >= 1; i--){
-            for(int j = 0; j < i; j++){
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - 1 - i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -14,15 +14,13 @@ public class bubbleSort {
                 }
             }
         }
-        System.out.println(arr);
     }
 
-    static void main(String[] args){
-        int arr[] = {3,1,5,4,2};
+    static void main() {
+        int [] arr = {3,1,5,4,2};
         bubble_sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
     }
 }
--- sorting/selectionSort.java
+++ sorting/selectionSort.java
@@ -1,7 +1,25 @@
 package sorting;
 
+import java.util.*;
+
 public class selectionSort {
-    public static void selection_sort(int[] arr){
-        
+    public static void selection_sort(int[] arr, int p){
+        for(int i = 0; i < p; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String [] args){
+        int [] nums = {29, 10, 14, 37, 13};
+        selection_sort(nums, 3);
     }
 }
